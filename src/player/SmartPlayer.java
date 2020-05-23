14b6e35af3376cff9a836d998fd0f4afb71bfd21
yanwu20 <13671010156@163.com>
2020-05-23 22:05:13 +1000

    Smart player

--- src/player/SmartPlayer.java
+++ src/player/SmartPlayer.java
@@ -1,5 +1,7 @@
 package player;
 
+import java.util.ArrayList;
+
 import ch.aplu.jcardgame.Card;
 import ch.aplu.jcardgame.Hand;
 import game.Whist;
@@ -24,19 +26,43 @@ public class SmartPlayer implements Player {
 		// TODO Auto-generated method stub
 		Suit leadSuit;
 		if(trick.getNumberOfCards()==0) {
-			selected = Whist.randomCard(trick);
-			leadSuit =(Suit) selected.getSuit();
+			return Whist.randomCard(trick);
 		}else {
 			leadSuit= (Suit) trick.get(0).getSuit();
 //			if player has lead suit card
 			if(this.hand.getNumberOfCardsWithRank(leadSuit)>0) {
+				ArrayList<Card> leadSuitList  = this.hand.getCardsWithSuit(leadSuit);
 				if(trick.getCardsWithSuit(trumps).size()>0) {
-					
-					selected = this.hand.getCardsWithSuit(leadSuit).get(0);
+					return leadSuitList.get(leadSuitList.size()-1);
+				}else {
+					return leadSuitList.get(0);
 				}
 			}
+//			if player do not have lead suit cards
 			else {
-				
+//				if player has trump suit card
+				if(this.hand.getNumberOfCardsWithSuit(trumps)>0) {
+					ArrayList<Card> trumpsCard = this.hand.getCardsWithSuit(trumps);
+//					someone plays trump suit card
+					if(trick.getNumberOfCardsWithSuit(trumps)>0) {
+						Card biggestTrumpCard = trick.getCardsWithSuit(trumps).get(0);
+//						select the suitable card
+						for(int i=0;i < trumpsCard.size();i++) {
+							if(trumpsCard.get(i).getValue()<=biggestTrumpCard.getValue()) {
+								return trumpsCard.get(i-1);
+							}
+						}
+					}
+//					none plays trump suit card
+					else {
+						return trumpsCard.get(trumpsCard.size()-1);
+					}
+					
+				}
+//				if player do not have trump suit card
+				else {
+					
+				}
 			}
 			
 		}
